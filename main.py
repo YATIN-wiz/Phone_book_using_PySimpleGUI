1afae8495d5beeeb54355195251a3a80fac953ec
Your Name <you@example.com>
2024-08-10 16:24:03 +0530

    deleting a number

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ def menu():
             [sg.Text("3. SEARCH FOR A NUMBER"), sg.Button("3")],
             [sg.Text("4. VIEW ALL THE CONTACTS"), sg.Button("4")],
             [sg.Text("5. TO VIEW THE CONTACTS OF A PARTICULAR RELATION "), sg.Button("5")],
-            [sg.Text("6. EXIT"), sg.Button("CANCEL")] ]
+            [sg.Text("6. EXIT"), sg.Button("6")] ]
     window = sg.Window('Window Title', layout)
     event = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':
-        return
+    window.close()
     return event[0]
 
 while True:
@@ -37,6 +36,38 @@ while True:
             cursor.execute("INSERT INTO CONTACTS VALUES (?,?,?)", (values[0], values[1], values[2]))
             conn.commit()
         sg.popup('Contact added successfully')
+        window.close()
+
+    elif choice == '2':
+        layout = [ [sg.Text("Enter name to delete:"),sg.Input()],
+                [sg.Button('Ok'), sg.Button('Cancel')] ]
+        window = sg.Window('2. DELETE A NUMBER', layout)
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+            break
+        else:
+            result = cursor.execute("DELETE FROM CONTACTS WHERE Name=?", (values[0],))
+            conn.commit()
+            if result.rowcount == 0:
+                sg.popup("Name not found!")
+            else:
+                sg.popup('Contact deleted successfully')
+        window.close()
+
+    elif choice == '3':
+        layout = [ [sg.Text("Enter name to search:"),sg.Input()],
+                [sg.Button('Ok'), sg.Button('Cancel')] ]
+        window = sg.Window('3. SEARCH FOR A NUMBER', layout)
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+            break
+        else:
+            cursor.execute("SELECT * FROM CONTACTS WHERE Name=?", (values[0],))
+            result = cursor.fetchone()
+            if result:
+                sg.popup(f"Name: {result[0]}, Number: {result[1]}, Relation: {result[2]}")
+            else:
+                sg.popup('Contact not found')
     
     
     
